f487f6d3832b8c6c49047d468f78f2ead3e25513
Michael Herland Valen <michaelhv@student.uia.no>
2020-12-29 14:56:55 +0100

    Added features

--- TimeIT/src/pycode/app.py
+++ TimeIT/src/pycode/app.py
@@ -1,31 +1,46 @@
-from flask import Flask, render_template, url_for
+from flask import render_template, url_for, flash, redirect, request
+from flaskblog import app, db, bcrypt
+from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.models import User, Post
+from flask_login import login_user, current_user, logout_user, login_required
 
 app = Flask(__name__, template_folder='templates', )
 
 
 @app.route('/')
 def homepage():
-    return render_template('html/HomePage.html')
+    return render_template('templates/templates/HomePage.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('html/LoginPage.html')
+    return render_template('templates/templates/LoginPage.html')
 
 
-@app.route('/register')
+@app.route("/register", methods=['GET', 'POST'])
 def register():
-    return render_template('html/RegisterAccountPage.html')
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('RegisterAccountPage.html.html', title='Register', form=form)
 
 
-@app.route('/newpassword')
-def forgotpassword():
-    return render_template('html/ForgotPasswordPage.html')
+
+@app.route('/getNewPassword')
+def getNewPassword():
+    return render_template('templates/templates/ForgotPasswordPage.html')
 
 
 @app.route('/confirmation')
 def confirm():
-    return render_template('html/ConfirmationPage.html')
+    return render_template('templates/templates/ConfirmationPage.html')
 
 
 if __name__ == '__main__':
--- TimeIT/start.sh
+++ TimeIT/start.sh
@@ -1,2 +1,6 @@
-cd ~/projects/TimeIT/TimeIT
+ls
+cd ~/projects/TimeIT/TimeIT || return
+chmod +x venv/bin/activate
 . venv/bin/activate
+cd ~/projects/TimeIT/TimeIT/src/pycode || return
+python3 app.py
